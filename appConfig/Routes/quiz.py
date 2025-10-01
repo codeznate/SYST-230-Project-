b671d7aab6f7cc06a1a2401accdea7f5a0f988d5
codeznate <natecrick0@gmail.com>
2025-09-30 23:00:48 -0400

    more config

--- appConfig/Routes/quiz.py
+++ appConfig/Routes/quiz.py
@@ -1,10 +1,14 @@
 # Quiz page for website
 
-from flask import Blueprint, render_template 
+from flask import Blueprint, render_template, request
 
-quiz_bp = Blueprint("quiz", __name__)
+quiz_bp = Blueprint("quiz", __name__, template_folder="templates")
 
 @quiz_bp.route("/", methods = ["GET", "POST"])
 
 def start_quiz():
+    if request.method == "POST":
+        answers = request.form
+        suggested_career = "Computer Science" if answers.get("q1") == "Technology" else "Art"
+        return render_template("results.html", career=suggested_career)
     return render_template("quiz.html")
