9e23d8b21e31130abb7d165e4a891a6f883e8485
codeznate <natecrick0@gmail.com>
2025-09-30 22:53:23 -0400

    debug

--- appConfig/Routes/quiz.py
+++ appConfig/Routes/quiz.py
@@ -1,10 +1,10 @@
-#Quiz page for website 
+# Quiz page for website
 
-#from flask import Blueprint, render_template 
+from flask import Blueprint, render_template 
 
-#quiz_bp = Blueprint("quiz", __name__)
+quiz_bp = Blueprint("quiz", __name__)
 
-#@quiz_bp.rout("/quiz")
+@quiz_bp.route("/", methods = ["GET", "POST"])
 
-#def start_quiz():
-    #return render_template("quiz.html")
+def start_quiz():
+    return render_template("quiz.html")
