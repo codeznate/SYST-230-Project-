c099477c0da67fcf27a48d9102d5edc9839e917c
codeznate <natecrick0@gmail.com>
2025-10-01 21:14:48 -0400

    expanding quiz, debugging, more html code and js code

--- appConfig/Routes/quiz.py
+++ appConfig/Routes/quiz.py
@@ -8,12 +8,36 @@ quiz_bp = Blueprint("quiz", __name__, template_folder="templates")
 
 def start_quiz():
     if request.method == "POST":
-        answer = request.form.get("q1")
-        if answer == "strongly" or answer == "agree":
-            suggested_career = "Engineering"
-        elif answer == "disagree":
-            suggested_career = "Arts"
+        answers = [request.form.get(f"q{i}") for i in range(1, 11)]
+
+        score = 0
+        for answer in answers:
+            if answer == "strongly":
+                score += 4
+            elif answer == "agree":
+                score += 3
+            elif answer == "disagree":
+                score += 2
+            else:
+                score += 1
+
+        if score >= 37:
+            suggested_major = "Engineering"
+        elif score >= 34:
+            suggested_major = "Computer Science"
+        elif score >= 31:
+            suggested_major = "Performing/Visual Arts"
+        elif score >= 28:
+            suggested_major = "Music Arts"
+        elif score >= 25: 
+            suggested_major = "Education"
+        elif score >= 21:
+            suggested_major = "Health Sciences"
+        elif score >= 18:
+            suggested_major = "Business"
+        elif score >= 15:
+            suggested_major = "Economics"
         else:
-            suggested_career = "Computer science"
-        return render_template("results.html", major=suggested_career)
+            suggested_major = "Entrepreneurship"
+        return render_template("results.html", major = suggested_major)
     return render_template("quiz.html")
